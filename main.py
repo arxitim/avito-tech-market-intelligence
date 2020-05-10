8c44c137d0889ca58fea2ec5b43df27f5ed139ff
arxitim <arseniy22848@gmail.com>
2020-05-10 19:19:14 +0300

    PEP-8 refactor; docstrings for public methods added

--- main.py
+++ main.py
@@ -6,12 +6,34 @@ app = FastAPI()
 
 
 @app.get("/generate")
-async def generating(secret: str, code_phrase: str):
+async def generating(secret: str, code_phrase: str) -> dict:
+    """
+    Processes request and makes a query
+    to the database for a record.
+
+    :param secret: secret message
+    :type secret: str
+    :param code_phrase: for access control
+    :type code_phrase: str
+    :return: response with secret_id
+    :rtype: dict
+    """
     db = MongoDB()
     return {"secret_id": db.create_secret(secret, code_phrase)}
 
 
 @app.get("/secrets/{secret_id}")
-async def geting(secret_id: str, code_phrase: str):
+async def geting(secret_id: str, code_phrase: str) -> dict:
+    """
+    Processes the request and makes a query
+    to the database for reading.
+
+    :param secret_id:
+    :type secret_id: str
+    :param code_phrase:
+    :type code_phrase: str
+    :return: decrypted secret or an error
+    :rtype: dict
+    """
     db = MongoDB()
     return {"secret": db.get_secret(secret_id, code_phrase)}
--- mongodb.py
+++ mongodb.py
@@ -6,35 +6,76 @@ import pymongo
 
 
 class MongoDB(object):
+    """
+    Responsible for connecting to the database,
+    writing the secret to it, reading the secret.
+
+    Built on the Singleton concept.
+    """
     _instance = None
+
+    server_auth = "mongodb+srv://arxitim:TKEq5BBqwC_85_M@" +\
+                  "avito-tech-mi-untpv" +\
+                  ".mongodb.net/test?retryWrites=true&w=majority"
 
     def __new__(cls):
         if not hasattr(cls, '_instance') or not cls._instance:
             cls._instance = super().__new__(cls)
-            connection = pymongo.MongoClient("mongodb+srv://arxitim:TKEq5BBqwC_85_M@avito-tech-mi-untpv"
-                                             + ".mongodb.net/test?retryWrites=true&w=majority")
+            connection = pymongo.MongoClient(cls.server_auth)
             cls._instance.db = connection["avito-tech"]
         return cls._instance
 
-    def create_secret(self, secret, code_phrase):
+    def create_secret(self, secret: str, code_phrase: str) -> str:
+        """
+        A method that writes a secret into a database
+        and returns a secret_id of that entry.
+
+        :param secret: secret message
+        :type secret: str
+        :param code_phrase: for access control
+        :type code_phrase: str
+        :return: secret_id in database
+        :rtype: str
+        """
         secrets = self._instance.db.secrets
-        # secrets.create_index("dt_create", expireAfterSeconds=5 * 60)
+
+        # index creation for an empty collection. CAN BE MODIFIED
+        # secrets.create_index("dt_create", expireAfterSeconds=10 * 60)
 
         timestamp = datetime.utcnow()
-        encrypted_secret = b64encode(secret.encode("UTF-8"))
+        encrypted_secret = b64encode(secret.encode("UTF-8"))  # do not store unencrypted data!
 
-        secret = {"secret": encrypted_secret, "code_phrase": code_phrase, "dt_create": timestamp}
+        secret = {
+            "secret": encrypted_secret,
+            "code_phrase": code_phrase,
+            "dt_create": timestamp
+        }
         secret_id = secrets.insert_one(secret).inserted_id
         return str(secret_id)
 
-    def get_secret(self, secret_id, code_phrase):
+    def get_secret(self, secret_id: str, code_phrase: str) -> str:
+        """
+        The method makes a query to the database,
+        checks the record access by code_phrase and,
+        if successful, decrypts and returns the secret text.
+
+        In case code_phrase is incorrect,
+        it returns the text with an error.
+
+        :param secret_id: secret_id in the database
+        :type secret_id: str
+        :param code_phrase: for access control
+        :type code_phrase: str
+        :return: secret message
+        :rtype: str
+        """
         secret_id = ObjectId(secret_id)
         secrets = self._instance.db.secrets
 
         secret = secrets.find_one({"_id": secret_id})
 
         if code_phrase == secret["code_phrase"]:
-            secrets.delete_one({"_id": secret_id})
+            secrets.delete_one({"_id": secret_id})  # the record can only be read once
             return b64decode(secret["secret"]).decode("UTF-8")
         else:
-            return {"error": "wrong code_phrase!"}
+            return "wrong code_phrase!"
